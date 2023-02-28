666ea7c92b4fe09a42e926002944abbc71f0e869
Cletus Kingdom <cletuskingdom@gmail.com>
2023-02-28 15:46:52 +0100

    No comment from Developer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "coingecko.com",
+				port: "",
+				pathname: "assets.coingecko.com",
+			},
+		],
+	},
+};
---
+++ pages/user/index.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import CoinList from "@/component/CoinList";
+
+export const getServerSideProps = async () => {
+	const res = await fetch(
+		"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+	);
+	const data = await res.json();
+
+	return {
+		props: {
+			data,
+		},
+	};
+};
+
+const UserDashboard = ({ data }) => {
+	return (
+		<div>
+			<h1>
+				<Link href="/">Back home</Link>
+			</h1>
+			<CoinList data={data} />
+		</div>
+	);
+};
+export default UserDashboard;
